9bebfa13515041d9483c1ecb17659a4517369eaf
Srujal Patel <srujal.patel02@gmail.com>
2019-01-29 12:03:36 +0530

    Add files via upload

---
+++ js/task1.completed.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	$('#productBox').sortable();
+	$('input[name=txtItem]').keyup(function (event) {
+		//here user press enter key button click event will fire
+		if(event.keyCode == 13) {
+			$('#btnAdd').click();	
+		}
+	});
+	$('#btnAdd').click(function () {
+		var item = $('#txtItem').val(); 
+		$('#productBox').append('<li class="list-group-item mb-3 bg-primary" id="' + item + '">' + item + '</li>');
+		$('#noProduct').empty();
+		$('input[name=txtItem]').val('');
+		console.log($('#productBox').contents().length);
+		checkMessage();
+	});
+	$(document).on('dblclick','li', function(){
+		var liId = $(this).attr('id') ;
+		// console.log(liId);
+		$('#' + liId).remove();
+        $(this).fadeOut('slow');    
+        checkMessage();
+    });
+    function checkMessage() {
+    	if($('#productBox').contents().length == 0) {
+			$('#noProduct').html("There is No Product");
+		} else {
+			$('#noProduct').html("You can delete list by double click on it");
+		}
+    }
+});
+
+
